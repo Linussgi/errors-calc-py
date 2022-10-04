0f71567a6d70bf4ceb339ac9448827fb1d045e64
Sunil <sgidda19@gmail.com>
2022-10-04 15:50:26 +0100

    cleaned up

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
-import re
 import operator
 
 ops = {
@@ -19,16 +18,22 @@ def calculate_error(groups, err_1, err_2):
         return [new_val, new_val * np.sqrt((err_1 / float(groups[0])) ** 2 + (err_2 / float(groups[2])) ** 2)]
     elif groups[1] == "^":
         new_val = ops[groups[1]](float(groups[0]), float(groups[2]))
-        return [new_val, np.abs(new_val) * np.abs(float(groups[2])) * (float(groups[0]) / err_1)]
+        return [new_val, np.abs(new_val) * np.abs(float(groups[2])) * (err_1 / float(groups[0]))]
     else:
         return "Operator not recognised"
 
 
 data = input("Enter calculation: ")
-err_num_1 = input("Enter error on first number: ")
-err_num_2 = input("Enter error on second number: ")
-
+err_num_1 = float(input("Enter error on first number: "))
 
 matches = data.split(" ")
 
-print(calculate_error(matches, float(err_num_1), float(err_num_2)))
+if matches[1] != "^":
+    err_num_2 = float(input("Enter error on second number: "))
+else:
+    err_num_2 = None
+
+value, error = calculate_error(matches, err_num_1, err_num_2)
+
+print(f"value: {value}")
+print(f"error: {error}")
