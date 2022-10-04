3a1d56937ef2356c7b63fcee6eb376c0b52ab56c
Sunil <sgidda19@gmail.com>
2022-10-04 02:22:11 +0100

    removed regex

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ ops = {
 }
 
 
-def calculate_error(re_groups, err_1, err_2):
-    if re_groups[1] == "+" or re_groups[1] == "-":
-        return [ops[re_groups[1]](float(re_groups[0]), float(re_groups[2])), err_1 + err_2]
-    elif re_groups[1] == "*" or re_groups[1] == "/":
-        new_val = ops[re_groups[1]](float(re_groups[0]), float(re_groups[2]))
-        return [new_val, new_val * np.sqrt((err_1 / float(re_groups[0])) ** 2 + (err_2 / float(re_groups[2])) ** 2)]
-    elif re_groups[1] == "^":
-        new_val = ops[re_groups[1]](float(re_groups[0]), float(re_groups[2]))
-        return [new_val, np.abs(new_val) * np.abs(float(re_groups[2])) * (float(re_groups[0]) / err_1)]
+def calculate_error(groups, err_1, err_2):
+    if groups[1] == "+" or groups[1] == "-":
+        return [ops[groups[1]](float(groups[0]), float(groups[2])), err_1 + err_2]
+    elif groups[1] == "*" or groups[1] == "/":
+        new_val = ops[groups[1]](float(groups[0]), float(groups[2]))
+        return [new_val, new_val * np.sqrt((err_1 / float(groups[0])) ** 2 + (err_2 / float(groups[2])) ** 2)]
+    elif groups[1] == "^":
+        new_val = ops[groups[1]](float(groups[0]), float(groups[2]))
+        return [new_val, np.abs(new_val) * np.abs(float(groups[2])) * (float(groups[0]) / err_1)]
     else:
         return "Operator not recognised"
 
@@ -29,6 +29,6 @@ err_num_1 = input("Enter error on first number: ")
 err_num_2 = input("Enter error on second number: ")
 
 
-matches = re.match(r"^([\d\.]+) ?([*^/+-]) ?([\d\.]+)$", data)
+matches = data.split(" ")
 
-print(calculate_error(matches.groups(), float(err_num_1), float(err_num_2)))
+print(calculate_error(matches, float(err_num_1), float(err_num_2)))
