2198345c7694380cec00f15d6ca0dd3afe631757
Sunil <sgidda19@gmail.com>
2022-10-04 01:59:16 +0100

    cleaned up

--- main.py
+++ main.py
@@ -2,31 +2,24 @@ import numpy as np
 import re
 import operator
 
-# ops["+"](1,1)
-
 ops = {
     "+" : operator.add,
     "-" : operator.sub,
     "*" : operator.mul,
     "/" : operator.truediv,
-    "^" : operator.pow,
+    "^" : operator.pow
 }
 
 
-def test(re_match, err_1, err_2):
-    if re_match.groups(0)[1] == "+" or re_match.groups(0)[1] == "-":
-        return [ops[re_match.groups(0)[1]](float(re_match.groups(0)[0]), float(re_match.groups(0)[2])), err_1 + err_2]
-
-
-def calculate_error(re_match, err_1, err_2):
-    if re_match.groups(0)[1] == "+" or re_match.groups(0)[1] == "-":
-        return [ops[re_match.groups(0)[1]](float(re_match.groups(0)[0]), float(re_match.groups(0)[2])), err_1 + err_2]
-    elif re_match.groups(0)[1] == "*" or re_match.groups(0)[1] == "/":
-        new_val = ops[re_match.groups(0)[1]](float(re_match.groups(0)[0]), float(re_match.groups(0)[2]))
-        return [new_val, new_val * np.sqrt((err_1 / float(re_match.groups(0)[0])) ** 2 + (err_2 / float(re_match.groups(0)[2])) ** 2)]
-    elif re_match.groups(0)[1] == "^":
-        new_val = ops[re_match.groups(0)[1]](float(re_match.groups(0)[0]), float(re_match.groups(0)[2]))
-        return [new_val, np.abs(new_val) * np.abs(float(re_match.groups(0)[2])) * (float(re_match.groups(0)[0]) / err_1)]
+def calculate_error(re_groups, err_1, err_2):
+    if re_groups[1] == "+" or re_groups[1] == "-":
+        return [ops[re_groups[1]](float(re_groups[0]), float(re_groups[2])), err_1 + err_2]
+    elif re_groups[1] == "*" or re_groups[1] == "/":
+        new_val = ops[re_groups[1]](float(re_groups[0]), float(re_groups[2]))
+        return [new_val, new_val * np.sqrt((err_1 / float(re_groups[0])) ** 2 + (err_2 / float(re_groups[2])) ** 2)]
+    elif re_groups[1] == "^":
+        new_val = ops[re_groups[1]](float(re_groups[0]), float(re_groups[2]))
+        return [new_val, np.abs(new_val) * np.abs(float(re_groups[2])) * (float(re_groups[0]) / err_1)]
     else:
         return "Operator not recognised"
 
@@ -38,4 +31,4 @@ err_num_2 = input("Enter error on second number: ")
 
 matches = re.match(r"^([\d\.]+) ?([*^/+-]) ?([\d\.]+)$", data)
 
-print(calculate_error(matches, float(err_num_1), float(err_num_2)))
+print(calculate_error(matches.groups(), float(err_num_1), float(err_num_2)))
